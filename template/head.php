8cd85ae5b7d1933cbdb853f0ca5fb65a7b84f435
RandMihkel <randmihkel@gmail.com>
2017-09-06 13:15:30 +0300

    Header mobile border-right now gone

--- template/head.php
+++ template/head.php
@@ -11,10 +11,17 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <style>
+        @media screen and (min-width: 420px) {
+            .navbar-divide {
+                padding-right: 1%;
+                border-right: solid white 1px;
+            }
+    </style>
 </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="#" style="padding-right: 1%; border-right: solid white 1px;">Auto24</a>
+    <a class="navbar-brand navbar-divide" href="index.php">Auto24</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -35,7 +42,7 @@
             </li>
             <!-- Hide if user is not Admin -->
             <li class="nav-item">
-                <a class="nav-link" href="#">Admin Panel</a>
+                <a class="nav-link" href="admin.php">Admin Panel</a>
             </li>
         </ul>
         <ul class="navbar-nav my-2 my-lg-0">
